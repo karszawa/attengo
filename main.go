c459fb4538e2dd7a1f96bb1a95cea3c08516a890
Hiroaki KARASAWA <siqvare@gmail.com>
2018-04-15 22:53:47 +0900

    Set up for heroku deploy

---
+++ main.go
@@ -0,0 +1,23 @@
+package attengo
+
+import (
+	"os"
+	"log"
+	"net/http"
+	"fmt"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":" + port, nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", r.URL.Path[1:])
+}
