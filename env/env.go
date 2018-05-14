ad80c3fd94dfd2fa9505e5ef7dc7de2aa3210e6a
Hiroaki KARASAWA <siqvare@gmail.com>
2018-05-14 19:58:19 +0900

    Load if .env file exist

--- env/env.go
+++ env/env.go
@@ -2,10 +2,13 @@ package env
 
 import (
 	"github.com/joho/godotenv"
+	"os"
 )
 
 func Load(filename ...string) {
-	if err := godotenv.Load(filename...); err != nil {
-		panic(err)
+	if _, err := os.Stat("./env"); err == nil {
+		if err := godotenv.Load(filename...); err != nil {
+			panic(err)
+		}
 	}
 }
