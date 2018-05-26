00dc08bde2fb0554e07f3002833b0a748f805fb3
Hiroaki KARASAWA <siqvare@gmail.com>
2018-05-26 17:19:23 +0900

    Refactor authentication

--- app/scripts/firebase/authentication.js
+++ app/scripts/firebase/authentication.js
@@ -1,9 +1,10 @@
 import firebase from './firebase';
 
 const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
 
 function signInWithPopup() {
-    return firebase.auth().signInWithPopup(provider);
+    return auth.signInWithPopup(provider);
 }
 
 export default signInWithPopup;
